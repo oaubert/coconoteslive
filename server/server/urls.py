fa5ad90c241ceb38492d99ca4c3cebab1aba9254
Olivier Aubert <contact@olivieraubert.net>
2013-11-15 15:42:42 +0100

    Redirect server root to app page + disable admin

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,9 +1,11 @@
+from django.views.generic import RedirectView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       url(r'^$', RedirectView.as_view(url="/static/index.html")),
                        url(r'^mla/', include('mla.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
+#                       url(r'^admin/', include(admin.site.urls)),
                        )
