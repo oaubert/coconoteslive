de275730ffceaf43dbc81d6d530fcb3ac82dc853
Olivier Aubert <contact@olivieraubert.net>
2013-11-15 15:42:22 +0100

    Redirect /mla root to app page

--- server/mla/urls.py
+++ server/mla/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import views
 
 urlpatterns = patterns('',
-                       url(r'^$', RedirectView.as_view(url="annotation/")),
+                       url(r'^$', RedirectView.as_view(url="/static/index.html")),
                        url(r'^annotation/?$', views.AnnotationList.as_view(), name='api-annotation-list'),
                        url(r'^annotation/(?P<pk>[0-9]+)/?$', views.AnnotationDetail.as_view(), name='api-annotation-detail'),
                        )
