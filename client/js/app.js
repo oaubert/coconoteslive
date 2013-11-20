55fccff9d6189dcf0dc9e912fb6c9c475f81e56b
Olivier Aubert <contact@olivieraubert.net>
2013-11-20 11:32:32 +0100

    Hardcode paths (yuk!), which temporarily solves issues on mobiles
    This should be properly fixed, but it is an interim measure for a forthcoming experiment.

--- client/js/app.js
+++ client/js/app.js
@@ -6,7 +6,7 @@ angular.module('mla', [
     'mla.restServices'
 ]).
 config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/annotations', {templateUrl: 'partials/annotation-list.html', controller: 'AnnotationListCtrl'});
-    $routeProvider.when('/annotations/:annotationId', {templateUrl: 'partials/annotation-detail.html', controller: 'AnnotationDetailCtrl'});
+    $routeProvider.when('/annotations', {templateUrl: '/static/partials/annotation-list.html', controller: 'AnnotationListCtrl'});
+    $routeProvider.when('/annotations/:annotationId', {templateUrl: '/static/partials/annotation-detail.html', controller: 'AnnotationDetailCtrl'});
     $routeProvider.otherwise({redirectTo: '/annotations'});
 }]);
