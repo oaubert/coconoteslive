37bf2b1ab379dbb7a0ae3859c670bc56c84b11c6
Olivier Aubert <contact@olivieraubert.net>
2014-06-10 15:02:40 +0200

    Modify routes syntax

--- client/js/app.js
+++ client/js/app.js
@@ -6,8 +6,8 @@ angular.module('mla', [
     'mla.restServices'
 ]).
 config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('annotations/shortcut/:shortcutId', { templateUrl: '../static/partials/annotation-list.html', controller: 'AnnotationListCtrl'});
-    $routeProvider.when('annotations/:annotationId', { templateUrl: '../static/partials/annotation-detail.html', controller: 'AnnotationDetailCtrl'});
+    $routeProvider.when('/shortcut/:shortcutId', { templateUrl: '../static/partials/annotation-list.html', controller: 'AnnotationListCtrl'});
+    $routeProvider.when('/annotations/:annotationId', { templateUrl: '../static/partials/annotation-detail.html', controller: 'AnnotationDetailCtrl'});
     $routeProvider.otherwise({ templateUrl: '../static/partials/annotation-list.html', controller: 'AnnotationListCtrl'});
 }])
 .config(['$httpProvider', function($httpProvider) {
