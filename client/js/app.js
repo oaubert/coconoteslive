65d4fe3e5cba748a3f9cabd9ca730691cf2447ba
Olivier Aubert <contact@olivieraubert.net>
2014-06-10 14:09:12 +0200

    Enable CSRF token for angular queries

--- client/js/app.js
+++ client/js/app.js
@@ -6,7 +6,11 @@ angular.module('mla', [
     'mla.restServices'
 ]).
 config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/annotations', {templateUrl: '/static/partials/annotation-list.html', controller: 'AnnotationListCtrl'});
-    $routeProvider.when('/annotations/:annotationId', {templateUrl: '/static/partials/annotation-detail.html', controller: 'AnnotationDetailCtrl'});
-    $routeProvider.otherwise({redirectTo: '/annotations'});
+    $routeProvider.when('annotations/shortcut/:shortcutId', { templateUrl: '../static/partials/annotation-list.html', controller: 'AnnotationListCtrl'});
+    $routeProvider.when('annotations/:annotationId', { templateUrl: '../static/partials/annotation-detail.html', controller: 'AnnotationDetailCtrl'});
+    $routeProvider.otherwise({ templateUrl: '../static/partials/annotation-list.html', controller: 'AnnotationListCtrl'});
+}])
+.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';    
 }]);
