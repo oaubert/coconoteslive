fa892788683499ed783d346df830d7839c1be107
Olivier Aubert <contact@olivieraubert.net>
2014-06-16 11:19:19 +0200

    Make mla the root application

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^/?$', RedirectView.as_view(url="mla/", permanent=False)),
-                       url(r'^mla/', include('mla.urls')),
+                       url(r'^/?$', include('mla.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
