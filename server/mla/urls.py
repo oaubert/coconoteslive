e71057f9711b7d485451c85856fda5a2daaed147
Olivier Aubert <contact@olivieraubert.net>
2014-06-20 16:49:45 +0200

    Try another variant for trailing root /

--- server/mla/urls.py
+++ server/mla/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.root, name='root'),
+                       url(r'^/$', views.root, name='root'),
                        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.dirname(os.path.normpath(__file__)) + '/static' }),
                        url(r'^(?P<group>\w+)/annotation/$', views.AnnotationList.as_view(), name='api-annotation-list'),
                        url(r'^(?P<group>\w+)/annotation/(?P<pk>[0-9]+)/$', views.AnnotationDetail.as_view(), name='api-annotation-detail'),
