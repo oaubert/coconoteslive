705f09c38548127e549e57fc1af0fd81d061858b
Olivier Aubert <contact@olivieraubert.net>
2014-06-20 14:24:05 +0200

    Define Annotation admin form

--- server/mla/admin.py
+++ server/mla/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Group, Annotation
 
 admin.site.register(Group)
-admin.site.register(Annotation)
+
+class AnnotationAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'creator', 'data', 'category', 'begin', 'created', 'source', 'group')
+    list_editable = ( 'creator', 'data', 'category', 'begin', 'source', 'group')
+    list_display_links = ( 'pk', )
+
+admin.site.register(Annotation, AnnotationAdmin)
