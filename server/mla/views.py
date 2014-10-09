oaubert/coconoteslive

commit 5e915962dfeb5a3cc90f51ac2ac6850c6d3e233b

Pass filter to client.html template

server/mla/views.py
@@ -71,13 +71,14 @@ display: none;
 """ % request.GET['filter']
     else:
         filteredcss = ""
-    return render_to_response('client.html', {
-        'group': group,
-        'shortcut': shortcut,
-        'customcss': filteredcss + "\n" + "\n".join(".category-%(identifier)s .annotation-category, .category-button.category-%(identifier)s { background-color: %(color)s }" % s.__dict__
-                               for s in Shortcut.objects.exclude(color="")),
-        'viewonly': 'viewonly' in request.GET,
-        'shortcuts': json.dumps(dict( (k, list(v)) for k, v in itertools.groupby(Shortcut.objects.order_by('group', 'position').values(), lambda x: x['group']) )),
+        return render_to_response('client.html', {
+            'filter': request.GET.get('filter', ''),
+            'group': group,
+            'shortcut': shortcut,
+            'customcss': filteredcss + "\n" + "\n".join(".category-%(identifier)s .annotation-category, .category-button.category-%(identifier)s { background-color: %(color)s }" % s.__dict__
+                                                        for s in Shortcut.objects.exclude(color="")),
+            'viewonly': 'viewonly' in request.GET,
+            'shortcuts': json.dumps(dict( (k, list(v)) for k, v in itertools.groupby(Shortcut.objects.order_by('group', 'position').values(), lambda x: x['group']) )),
     }, context_instance=RequestContext(request))
 
 def root(request, *p):

commit 5ea5578506d5bc7eac6488aad42663899c0a0170

Fix wrong indent, introduced by 5e915962dfeb5a3cc90f51ac2ac6850c6d3e233b

server/mla/views.py
@@ -74,14 +74,14 @@ display: none;
 """ % request.GET['filter']
     else:
         filteredcss = ""
-        return render_to_response('client.html', {
-            'filter': request.GET.get('filter', ''),
-            'group': group,
-            'shortcut': shortcut,
-            'customcss': filteredcss + "\n" + "\n".join(".category-%(identifier)s .annotation-category, .category-button.category-%(identifier)s { background-color: %(color)s }" % s.__dict__
-                                                        for s in Shortcut.objects.exclude(color="")),
-            'viewonly': 'viewonly' in request.GET,
-            'shortcuts': json.dumps(dict( (k, list(v)) for k, v in itertools.groupby(Shortcut.objects.order_by('group', 'position').values(), lambda x: x['group']) )),
+    return render_to_response('client.html', {
+        'filter': request.GET.get('filter', ''),
+        'group': group,
+        'shortcut': shortcut,
+        'customcss': filteredcss + "\n" + "\n".join(".category-%(identifier)s .annotation-category, .category-button.category-%(identifier)s { background-color: %(color)s }" % s.__dict__
+                                                    for s in Shortcut.objects.exclude(color="")),
+        'viewonly': 'viewonly' in request.GET,
+        'shortcuts': json.dumps(dict( (k, list(v)) for k, v in itertools.groupby(Shortcut.objects.order_by('group', 'position').values(), lambda x: x['group']) )),
     }, context_instance=RequestContext(request))
 
 def root(request, *p):